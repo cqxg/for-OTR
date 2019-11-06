d4664077b03f1e244d68a629133b7f5bd2bfd6f2
cqxg <cqxg@yandex.ru>
2019-11-06 17:52:27 +0300

    feat: add third task

---
+++ 3/index.js
@@ -0,0 +1,28 @@
+
+const Generate = function () {
+
+    const request = document.querySelector('.request');
+    const res = document.querySelector('.res');
+    request.addEventListener('click', e => {
+
+        const xhr = new XMLHttpRequest();            //В данном задании я не совсем понял, что от меня требовалось.
+        xhr.open("GET", "/bar/foo.txt", true);      //Организовал это так. Пример моей работы с запросами
+        xhr.onload = function (e) {                //Вы можете посмотреть по ссылке https://github.com/cqxg
+            if (xhr.readyState === 4) {           //В частности, например https://github.com/cqxg/search
+                if (xhr.status === 200) {
+                    console.log(xhr.responseText);
+                    res.innerHTML = xhr.responseText;
+                } else {
+                    console.error(xhr.statusText);
+                }
+            }
+        };
+        xhr.onerror = function (e) {
+            console.error(xhr.statusText);
+        };
+        xhr.send(null);
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", Generate);
