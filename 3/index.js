46b58b3a30b5cf84eb6b51d6edc6d4a8cc033559
cqxg <cqxg@yandex.ru>
2019-11-06 17:54:17 +0300

    fix: simple fix

--- 3/index.js
+++ 3/index.js
@@ -5,10 +5,10 @@ const Generate = function () {
     const res = document.querySelector('.res');
     request.addEventListener('click', e => {
 
-        const xhr = new XMLHttpRequest();            //В данном задании я не совсем понял, что от меня требовалось.
-        xhr.open("GET", "/bar/foo.txt", true);      //Организовал это так. Пример моей работы с запросами
-        xhr.onload = function (e) {                //Вы можете посмотреть по ссылке https://github.com/cqxg
-            if (xhr.readyState === 4) {           //В частности, например https://github.com/cqxg/search
+        const xhr = new XMLHttpRequest();              //В данном задании я не совсем понял, что от меня требовалось.
+        xhr.open("GET", "/response/res.txt", true);   //Организовал это так. Пример моей работы с запросами
+        xhr.onload = function (e) {                  //Вы можете посмотреть по ссылке https://github.com/cqxg
+            if (xhr.readyState === 4) {             //В частности, например https://github.com/cqxg/search
                 if (xhr.status === 200) {
                     console.log(xhr.responseText);
                     res.innerHTML = xhr.responseText;
