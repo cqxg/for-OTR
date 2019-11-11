6dfaa3a60c809c0122473c9cebe707b1ca7ccb7b
cqxg <cqxg@yandex.ru>
2019-11-11 23:36:47 +0300

    refactoring for best practice

--- 4/content.js
+++ 4/content.js
@@ -1,29 +1,102 @@
 
 const crossDomainApp = function () {
+    const LOCAL_STORAGE_KEY = 'table';
+    const INITIAL_STATE = [];
 
-    change.addEventListener('click', function (e) {
-        let row = table.insertRow();
+    const table = document.querySelector('#table');
+    const change = document.querySelector('#change');
+    const input = document.querySelector('#input');
+    const deleteButton = '<input type="button" value="DELETE" class="del"/>';
+
+    // ---------------------------- Local Storage  ----------------------------------
+    const getData = () => JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || INITIAL_STATE;
+    const setData = (data) => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
+    // ------------------------------------------------------------------------------
+
+    // ---------------------------- Table Manipulation ------------------------------
+    const renderTable = (data) => {
+        if (data) {
+            data.forEach(row => addRow(row.value));
+        }
+    }
+
+    const addRow = (value) => {
+        const row = table.insertRow();
+        row.setAttribute('id', `${value}_${table.rows.length}`);
+
         row.insertCell().innerHTML = row.sectionRowIndex;
-        row.insertCell().innerHTML = input.value;
-        row.insertCell().innerHTML = '<input type="button" value="DELETE" class="del"/>';
-        input.value = "";
-        localStorage.setItem("item", document.getElementsByTagName('table')[0].innerHTML);
-        console.log('Данные добавлены !');
-    });
+        row.insertCell().innerHTML = value;
+        row.insertCell().innerHTML = deleteButton;
+        return row;
+    };
 
-    table.addEventListener('click', function (e) {
-        if (e.target.classList.contains('del')) {
-            e.target.parentNode.parentNode.remove();
-            localStorage.setItem("item", document.getElementsByTagName('table')[0].innerHTML);
+    const deleteRow = (target) => {
+        const row = target.parentNode.parentNode;
+        row.remove();
+    }
+    // ------------------------------------------------------------------------------
+
+    const clearInput = () => {
+        input.value = '';
+    }
+
+    // ----------------------------- LS Manipulation --------------------------------
+    const addItem = (item) => {
+        const tableData = getData();
+        tableData.push(item);
+        setData(tableData);
+    };
+
+    const removeItem = (item) => {
+        const tableData = getData();
+        const newTableData = tableData.filter((row) => row.key !== item.key);
+        setData(newTableData);
+    }
+    // ------------------------------------------------------------------------------
+
+    const changeHadler = () => {
+        const value = input.value;
+
+        const row = addRow(value);
+
+        const item = {
+            key: row.getAttribute('id'),
+            value: value,
+        }
+        addItem(item);
+
+        clearInput();
+
+        console.log('Данные добавлены !');
+    };
+
+    const deleteHandler = (e) => {
+        const target = e.target;
+        const isDeleteButton = target.classList.contains('del');
+
+        if (isDeleteButton) {
+            const row = target.parentNode.parentNode;
+
+            const item = {
+                key: row.getAttribute('id'),
+                value: row.childNodes[1].innerText,
+            }
+            removeItem(item);
+
+            deleteRow(target);
+
             console.log('Данные удалены !');
         };
-    });
+    };
 
-    window.addEventListener("load", function () {
-        if (localStorage.getItem('item'));
-        document.getElementsByTagName('table')[0].innerHTML = localStorage.getItem('item')
-    });
+    const init = () => {
+        const tableData = getData();
+        renderTable(tableData);
+    }
 
+    change.addEventListener('click', changeHadler);
+    table.addEventListener('click', deleteHandler);
+    window.addEventListener('load', init);
 };
 
-document.addEventListener("DOMContentLoaded", crossDomainApp);
+document.addEventListener('DOMContentLoaded', crossDomainApp);
--- 4/index.js
+++ 4/index.js
@@ -3,10 +3,12 @@ const crossDomainApp = function () {
     const myIframe = document.getElementById('myIframe');
     const change = document.getElementById('change');
     const input = document.getElementById('input');
+    const get = document.getElementById('get');
+
 
     change.addEventListener('click', e => {
 
-        let row = myIframe.contentDocument.querySelector('#table').insertRow();
+        const row = myIframe.contentDocument.querySelector('#table').insertRow();
         row.insertCell().innerHTML = row.sectionRowIndex;
         row.insertCell().innerHTML = input.value;
         row.insertCell().innerHTML = '<input type="button" value="DELETE" class="del"/>';
@@ -15,12 +17,55 @@ const crossDomainApp = function () {
         console.log('Данные добавлены !');
     });
 
+    get.addEventListener('click', e => {
+
+        let url = 'http://127.0.0.1:5500/content.html';
+        let XHR = window.XDomainRequest || window.XMLHttpRequest
+        let xhr = new XHR();
+        xhr.open('GET', url, true);
+        xhr.onload = function () {
+            document.querySelector('.output').innerHTML = xhr.response.localStorage.getItem('item');
+        }
+        xhr.onerror = function () {
+            alert("Error")
+        }
+        xhr.send()
+
+    });
+
     myIframe.contentWindow.addEventListener("load", function () {
         if (localStorage.getItem('item'));
-        myIframe.contentDocument.getElementsByTagName('table')[0].innerHTML = localStorage.getItem('item')
+        myIframe.contentDocument.getElementsByTagName('table')[0].innerHTML = localStorage.getItem('item');
     });
 
 
 };
+
+// const ls = {
+//     item: `{
+//         value: '',
+//         callback: () => {},
+//     }`,
+// };
+
+// const readStorage = () => {
+//     const item = localStorage.getItem();
+//     if (item) {
+//         console.log(item.value);
+//         item.callback();
+//     } else {
+//         console.log('Read unsuccessful');
+//     }
+// }
+
+// const writeStorage = (value) => {
+//     const item = localStorage.setItem(value);
+//     if (item) {
+//         console.log(item.value);
+//         item.callback();
+//     } else {
+//         console.log('Write unsuccessful');
+//     }
+// }
 
 document.addEventListener("DOMContentLoaded", crossDomainApp);
