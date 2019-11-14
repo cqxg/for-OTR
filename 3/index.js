4f4b3fb900aa76d72ae7a97b1aac42abf213c7c4
cqxg <cqxg@yandex.ru>
2019-11-14 14:09:57 +0300

    fix: third task redone

--- 3/index.js
+++ 3/index.js
@@ -1,28 +1,29 @@
 
-const Generate = function () {
+const Generate = () => {
 
     const request = document.querySelector('.request');
     const res = document.querySelector('.res');
-    request.addEventListener('click', e => {
 
-        const xhr = new XMLHttpRequest();              //В данном задании я не совсем понял, что от меня требовалось.
-        xhr.open("GET", "/response/res.txt", true);   //Организовал это так. Пример моей работы с запросами
-        xhr.onload = function (e) {                  //Вы можете посмотреть по ссылке https://github.com/cqxg
-            if (xhr.readyState === 4) {             //В частности, например https://github.com/cqxg/search
-                if (xhr.status === 200) {
-                    console.log(xhr.responseText);
-                    res.innerHTML = xhr.responseText;
-                } else {
-                    console.error(xhr.statusText);
-                }
-            }
-        };
-        xhr.onerror = function (e) {
-            console.error(xhr.statusText);
-        };
-        xhr.send(null);
-    });
+    firstRequest = () => {
+        fetch('/response/res.json')
+            .then(res => res.json())
+            .then(response => {
+                secondRequest()
+            })
+            .catch(err => console.log(err));
+    };
 
-}
+    secondRequest = () => {
+        fetch('/response/res.json')
+            .then(res => res.json())
+            .then(response => {
+                console.log(response.data);
+                res.innerHTML = response.data;
+            })
+            .catch(err => console.log(err));
+    };
+
+    request.addEventListener('click', firstRequest);
+};
 
 document.addEventListener("DOMContentLoaded", Generate);
